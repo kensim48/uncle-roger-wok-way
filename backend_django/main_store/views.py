27625bd660ace1fcb3b66580dbb45ff87e6bf555
Kenneth Sim <kensim28@hotmail.com>
2021-08-12 14:25:08 +0800

    Added POST req for items

--- backend_django/main_store/views.py
+++ backend_django/main_store/views.py
@@ -1,11 +1,13 @@
+from re import T
 from django.http import JsonResponse
 from django.shortcuts import render
 from rest_framework.decorators import api_view
+from rest_framework.parsers import JSONParser
 from .models import *
 from .serializers import *
 
 # Create your views here.
-@api_view(["GET"])
+@api_view(["GET", "POST"])
 def item_listing(request):
     if request.method == "GET":
         id = request.query_params.get("id", None)
@@ -15,4 +17,20 @@ def item_listing(request):
         if len(item_listings) == 0:
             return JsonResponse({}, status=404)
         serializer = ItemListingSerializer(item_listings, many=True)
-        return JsonResponse({"data": serializer.data}, safe=False)
+        return JsonResponse({"data": serializer.data})
+    
+    # For both creating and modifying
+    elif request.method == "POST":
+        data = JSONParser().parse(request)
+        if "id" in data:
+            id = data["id"]
+            item_listing = ItemListing.objects.get(id=id)
+            serializer = ItemListingSerializer(item_listing, data=data)
+            statuscode = 200
+        else:
+            serializer = ItemListingSerializer(data=data)
+            statuscode = 201
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=statuscode)
+        return JsonResponse(serializer.errors, status=400)
