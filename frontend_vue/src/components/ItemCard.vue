9505dce3469f94841b1044db8beb1e79cb624dcd
Kenneth Sim <kensim28@hotmail.com>
2021-08-12 20:15:37 +0800

    Main page done

---
+++ frontend_vue/src/components/ItemCard.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-card>
+    <v-img
+      class="white--text align-end"
+      gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+      height="200px"
+    >
+    </v-img>
+    <v-card-title>
+      {{ item.name }}
+    </v-card-title>
+    <v-card-subtitle> ${{ item.price.toFixed(2) }} </v-card-subtitle>
+
+    <v-card-actions>
+      <div class="ml-3" v-if="item.quantity != 0">
+        {{ item.quantity }} in cart
+      </div>
+      <v-spacer></v-spacer>
+      <div v-if="item.quantity == 0">
+        <v-btn fab dark small color="primary">
+          <v-icon>mdi-cart-plus</v-icon>
+        </v-btn>
+      </div>
+      <div v-else>
+        <v-btn icon>
+          <v-icon>mdi-minus-circle-outline</v-icon>
+        </v-btn>
+        <v-btn icon>
+          <v-icon>mdi-plus-circle</v-icon>
+        </v-btn>
+      </div>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    item: Object,
+  },
+};
+</script>
--- frontend_vue/src/views/Home.vue
+++ frontend_vue/src/views/Home.vue
@@ -1,18 +1,12 @@
 <template>
   <v-app id="inspire">
-    <v-app-bar
-      app
-    >
-      <v-toolbar-title>Wok's Way</v-toolbar-title>
+    <v-app-bar app color="primary">
+      <v-toolbar-title>Wok Way</v-toolbar-title>
 
       <v-spacer></v-spacer>
 
-      <v-btn class="ma-2" >
-        Register
-      </v-btn>
-      <v-btn class="ma-2" color="primary">
-        Login
-      </v-btn>
+      <v-btn class="ma-2"> Register </v-btn>
+      <v-btn class="ma-2" color="secondary"> Login </v-btn>
     </v-app-bar>
 
     <v-main>
@@ -24,12 +18,13 @@
           height="200"
         />
         <v-row>
-          <v-col
-            v-for="n in 24"
-            :key="n"
-            cols="4"
-          >
-            <v-card height="200"></v-card>
+          <v-col>
+            <div>Top Products</div>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col v-for="n in 24" :key="n" cols="4">
+            <ItemCard :item="item" />
           </v-col>
         </v-row>
       </v-container>
@@ -38,7 +33,17 @@
 </template>
 
 <script>
-  export default {
-    //
-  }
+import ItemCard from "../components/ItemCard.vue";
+export default {
+  components: {
+    ItemCard,
+  },
+  data: () => ({
+    item: {
+      name: "hello",
+      price: 4,
+      quantity: 0,
+    },
+  }),
+};
 </script>
