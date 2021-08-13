678f7bd1194e36867e8f673e491c83bcf38e172b
Kensim48 <kensim28@hotmail.com.e>
2021-08-13 10:21:27 +0800

    Added docker-related files

--- backend_django/wok_way/settings.py
+++ backend_django/wok_way/settings.py
@@ -78,9 +78,17 @@ WSGI_APPLICATION = "wok_way.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "db",
+        "PORT": 5432,
     }
+    #     "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # }
 }
 
 
